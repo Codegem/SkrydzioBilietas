0b93a1cafd63250b1cf21d1836dacfb3fda094dd
Codegem <evaldaskis@gmail.com>
2020-09-07 09:37:58 +0300

    Done with Required

--- index.view.php
+++ index.view.php
@@ -11,7 +11,7 @@
             <?php foreach($masyvas as $elementai => $select):?>
                 <?php if($elementai == 'skrydzionr'):?>
                     <label for="skrydzionr">Skrydzio Nr</label>
-                        <select name="skrydzionr" id="skrydzionr" require>
+                        <select name="skrydzionr" id="skrydzionr" required>
                             <option disabled selected>--Pasirinkite Skrydzio Nr--</option>
                                 <?php for($i=0; $i<count($select); $i++):?>
                                     <option value=<?=$select[$i]?>> 
@@ -22,7 +22,7 @@
                 <?php endif?>
                 <?php if($elementai == 'iskur'):?>
                     <label for="iskur">Is Kur Skrendate</label>
-                        <select name="iskur" id="iskur" require>
+                        <select name="iskur" id="iskur" required>
                             <option disabled selected>--Is Kur Skrisite?--</option>
                                     <?php for($i=0; $i<count($select); $i++):?>
                                     <option value=<?=$select[$i]?>> 
@@ -33,7 +33,7 @@
                 <?php endif?>
                 <?php if($elementai == 'ikur'):?>
                     <label for="ikur">I Kur Skrendate</label>
-                        <select name="ikur" id="ikur" require>
+                        <select name="ikur" id="ikur" required>
                             <option disabled selected>--I Kur Skrisite?--</option>
                                     <?php for($i=0; $i<count($select); $i++):?>
                                     <option value=<?=$select[$i]?>> 
@@ -44,7 +44,7 @@
                 <?php endif?>
                 <?php if($elementai == 'bagazas'):?>
                     <label for="bagazas">Bagazo Dydis</label>
-                        <select name="svoris" id="bagazas" require>
+                        <select name="svoris" id="bagazas" required>
                             <option disabled selected>--Pasirinkite Bagaza--</option>
                                     <?php for($i=0; $i<count($select); $i++):?>
                                     <option value=<?=$select[$i]?>> 
@@ -56,10 +56,10 @@
             <?php endforeach?>
 <!-- --------------------------kitka-------------------------- -->
     <label for="kodas">Asmens Kodas</label>
-        <input type="number" name="kodas" placeholder="Asmens Kodas" require>
+        <input type="number" name="kodas" placeholder="Asmens Kodas" required>
     <label for="vardas_pavarde">Vardas, Pavarde</label>
-        <input type="text" name="vardas" placeholder="Vardas" require>
-        <input type="text" name="pavarde" placeholder="Pavarde" require>
+        <input type="text" name="vardas" placeholder="Vardas" required>
+        <input type="text" name="pavarde" placeholder="Pavarde" required>
     <label for="pastabos">Pastabos</label>
         <textarea name="pastabos" id="pastabos" cols="65" rows="10" placeholder="Jusu Pastabos.."></textarea>
     <button name="rezervuoti" type="submit">Rezervuoti</button>
